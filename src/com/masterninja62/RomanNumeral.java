ec9e2623b4f0be059800ded9dc49b0045a39cc49
MasterNinja62 <masterninja62@gmail.com>
2015-05-16 17:41:43 -0400

    Added/completed test cases to check for valid/invalid single character roman numerals.

--- src/com/masterninja62/RomanNumeral.java
+++ src/com/masterninja62/RomanNumeral.java
@@ -1,8 +1,25 @@
 package com.masterninja62;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumeral {
+    private static Map valid_characters = new HashMap<Character, Integer>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
 
     public boolean contains_valid_characters(String roman_numerals) {
+        for(int i = 0; i < roman_numerals.length(); i++) {
+            if (!valid_characters.containsKey(roman_numerals.charAt(i)))
+                return false;
+        }
         return true;
     }
 }
--- test/Roman_To_Arabic.java
+++ test/Roman_To_Arabic.java
@@ -1,14 +1,30 @@
-/**
- * Created by MasterNinja on 5/13/2015.
- */
 import com.masterninja62.RomanNumeral;
 import org.junit.Test;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class Roman_To_Arabic {
-        @Test
-        public void valid_roman_numeral_character_detected() {
-            RomanNumeral romanNumeral = new RomanNumeral();
-            assertEquals(true, romanNumeral.contains_valid_characters("I"));
-        }
+    @Test
+    public void valid_roman_numeral_character_detected() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        assertEquals(true, romanNumeral.contains_valid_characters("I"));
+        assertEquals(true, romanNumeral.contains_valid_characters("V"));
+        assertEquals(true, romanNumeral.contains_valid_characters("X"));
+        assertEquals(true, romanNumeral.contains_valid_characters("L"));
+        assertEquals(true, romanNumeral.contains_valid_characters("C"));
+        assertEquals(true, romanNumeral.contains_valid_characters("D"));
+        assertEquals(true, romanNumeral.contains_valid_characters("M"));
+    }
+
+    @Test
+    public void invalid_roman_numeral_character_detected() {
+        RomanNumeral romanNumeral = new RomanNumeral();
+        assertEquals(false, romanNumeral.contains_valid_characters("A"));
+        assertEquals(false, romanNumeral.contains_valid_characters("F"));
+        assertEquals(false, romanNumeral.contains_valid_characters("1"));
+        assertEquals(false, romanNumeral.contains_valid_characters("E"));
+        assertEquals(false, romanNumeral.contains_valid_characters("^"));
+        assertEquals(false, romanNumeral.contains_valid_characters("P"));
+        assertEquals(false, romanNumeral.contains_valid_characters("J"));
+    }
 }
